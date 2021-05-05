2def3b0ef54ed01e080f976c3a97b27240863a0f
TaliyaR <taliya200499@gmail.com>
2021-05-05 18:35:28 +0300

    Finished main screen layout

--- app/src/main/java/com/example/restaurant/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/restaurant/ui/splash/SplashActivity.kt
@@ -1,13 +1,16 @@
 package com.example.restaurant.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurant.MainActivity
 import com.example.restaurant.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
