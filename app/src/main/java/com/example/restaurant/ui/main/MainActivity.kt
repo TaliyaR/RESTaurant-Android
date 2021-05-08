d1cba98519c1ad88f4218468d014931f3c88369a
TaliyaR <taliya200499@gmail.com>
2021-05-08 19:45:30 +0300

    Init Hilt

--- app/src/main/java/com/example/restaurant/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/restaurant/ui/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.restaurant.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.restaurant.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
